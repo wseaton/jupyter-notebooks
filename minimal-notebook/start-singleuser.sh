317c35f8aaad8f0627edb57bd721030c2b8ffa5c
Václav Pavlín <vaclav.pavlin@gmail.com>
2018-05-23 10:40:08 +0200

    Pre-clone/pull repositories defined in JUPYTER_PRELOAD_REPOS

--- minimal-notebook/start-singleuser.sh
+++ minimal-notebook/start-singleuser.sh
@@ -2,6 +2,21 @@
 
 set -eo pipefail
 
+
+if [ -n "${JUPYTER_PRELOAD_REPOS}" ]; then
+    for repo in `echo ${JUPYTER_PRELOAD_REPOS} | tr ',' ' '`; do
+        echo "Checking if repository $repo exists locally"
+        REPO_DIR=$(basename ${repo})
+        if [ -d "${REPO_DIR}" ]; then
+            pushd ${REPO_DIR}
+            GIT_SSL_NO_VERIFY=true git pull --ff-only
+            popd
+        else
+            GIT_SSL_NO_VERIFY=true git clone ${repo} ${REPO_DIR}
+        fi
+    done
+fi
+
 if [[ "$NOTEBOOK_ARGS $@" != *"--ip="* ]]; then
   NOTEBOOK_ARGS="--ip=0.0.0.0 $NOTEBOOK_ARGS"
 fi
