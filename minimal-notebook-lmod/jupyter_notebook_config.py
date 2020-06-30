865e1f47670990176db3dcb8ab2c7eb8972be7fe
Guillaume Moutier <guillaume.moutier@gmail.com>
2020-06-30 16:27:41 -0400

    adapt

--- minimal-notebook-lmod/jupyter_notebook_config.py
+++ minimal-notebook-lmod/jupyter_notebook_config.py
@@ -16,3 +16,37 @@ image_config_file = '/opt/app-root/src/.jupyter/jupyter_notebook_config.py'
 if os.path.exists(image_config_file):
     with open(image_config_file) as fp:
         exec(compile(fp.read(), image_config_file, 'exec'), globals())
+
+# server-proxy configuration
+c.ServerProxy.servers = {
+    'vscode': {
+        'command': [
+            'code-server',
+            '--auth=none',
+            '--disable-telemetry',
+            '--port={port}'
+        ],
+        'timeout': 20,
+        'launcher_entry': {
+            'title': 'VS Code',
+            'enabled': False
+        }
+    },
+    'rstudio': {
+        'command': [
+            'rserver',
+            '--www-port={port}',
+            '--www-frame-origin=same',
+            '--user={user}',
+            '--auth-none=1',
+            '--server-daemonize=0',
+            '--server-working-dir=/opt/app-root/src'
+        ],
+        'timeout': 20,
+        'absolute_url': False,
+        'launcher_entry': {
+            'title': 'RStudio',
+            'enabled': False
+        }
+    },
+}
